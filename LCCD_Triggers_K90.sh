cdc0cd3bcea95702031492d0d25c185adb98d403
JuPiTeR <jupiter126@gmail.com>
2012-11-09 18:29:18 +0100

    new file:   LCCD_Triggers_K90.sh 	modified:   LCCD_core.sh 	modified:   getscancodes.o

---
+++ LCCD_Triggers_K90.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+if [ "x$1" = "x" ]; then # Launch with arguments if any
+        echo "No arguments detected, exiting"
+        exit 0
+elif [ $1 = "switch" ]; then
+        f_switch
+elif [ $1 = "create" ]; then
+        f_create "$2"
+elif [ $1 = "list" ]; then
+        f_list
+elif [ $1 = "458960" ]; then
+        f_G01
+        exit
+elif [ $1 = "458961" ]; then
+        f_G02
+        exit
+elif [ $1 = "458962" ]; then
+        f_G03
+        exit
+elif [ $1 = "458963" ]; then
+        f_G04
+        exit
+elif [ $1 = "458964" ]; then
+        f_G05
+        exit
+elif [ $1 = "458965" ]; then
+        f_G06
+        exit
+elif [ $1 = "458966" ]; then
+        f_G07
+        exit
+elif [ $1 = "458967" ]; then
+        f_G08
+        exit
+elif [ $1 = "458969" ]; then
+        f_G10
+        exit
+elif [ $1 = "458970" ]; then
+        f_G11
+        exit
+elif [ $1 = "458971" ]; then
+        f_G12
+        exit
+elif [ $1 = "458972" ]; then
+        f_G13
+        exit
+elif [ $1 = "458973" ]; then
+        f_G14
+        exit
+elif [ $1 = "458974" ]; then
+        f_G15
+        exit
+elif [ $1 = "cheatsheet" ] || [ $1 = "458975" ]; then
+        f_G16
+        exit
+else
+        echo 'Unknown Argument.'
+fi
--- LCCD_core.sh
+++ LCCD_core.sh
@@ -166,62 +166,6 @@ f_debug $fonction
 f_libnotify 12000 "CheatSheet for $activelayout layout" "$(cat $(ls -a|grep "$activelayout" |grep -v "~"|cut -f2 -d">")|grep "_-_"|grep -v cheatsheet|cut -f2 -d"=")"
 }
 # End of declarations, program "entry point"
-if [ "x$1" = "x" ]; then # Launch with arguments if any
-	echo "No arguments detected, exiting"
-	exit 0
-elif [ $1 = "switch" ]; then
-	f_switch
-elif [ $1 = "create" ]; then
-	f_create "$2"
-elif [ $1 = "list" ]; then
-	f_list
-elif [ $1 = "458960" ]; then
-	f_G01
-	exit
-elif [ $1 = "458961" ]; then
-	f_G02
-	exit
-elif [ $1 = "458962" ]; then
-	f_G03
-	exit
-elif [ $1 = "458963" ]; then
-	f_G04
-	exit
-elif [ $1 = "458964" ]; then
-	f_G05
-	exit
-elif [ $1 = "458965" ]; then
-	f_G06
-	exit
-elif [ $1 = "458966" ]; then
-	f_G07
-	exit
-elif [ $1 = "458967" ]; then
-	f_G08
-	exit
-elif [ $1 = "458969" ]; then
-	f_G10
-	exit
-elif [ $1 = "458970" ]; then
-	f_G11
-	exit
-elif [ $1 = "458971" ]; then
-	f_G12
-	exit
-elif [ $1 = "458972" ]; then
-	f_G13
-	exit
-elif [ $1 = "458973" ]; then
-	f_G14
-	exit
-elif [ $1 = "458974" ]; then
-	f_G15
-	exit
-elif [ $1 = "cheatsheet" ] || [ $1 = "458975" ]; then
-	f_G16
-	exit
-else
-	echo 'Unknown Argument.'
-fi
+source $directory/LCCD_Triggers_K90.sh #Loads the right triggers for device
 exit
 ##### If you like this script, please consider donating something to the Free Software Foundation, Debian, OpenBSD, ... or any other valuable open source project! Thanks ;)
